262963be9e75676e03abfb73a8aca7eb22cfa54f
junyub301 <junyub0301@gmail.com>
2024-03-30 19:12:18 +0900

    Feat: 카드신청 part2
    - 약관 추가

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -5,10 +5,13 @@ import { useState } from 'react'
 
 export default function Apply() {
   const [step, setStep] = useState<number>(0)
+  const handleTermsChange = (terms: string[]) => {
+    console.log('🚀 ~ handleTermsChange ~ terms:', terms)
+  }
 
   return (
     <div>
-      {step === 0 && <Terms />}
+      {step === 0 && <Terms onNext={handleTermsChange} />}
       {step === 1 && <BasicInfo />}
       {step === 2 && <CardInfo />}
     </div>
