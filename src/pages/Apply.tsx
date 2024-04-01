1206bebbb354c57a6ddbbb1c866f8b2c3fc1fb3f
junyub301 <junyub0301@gmail.com>
2024-04-01 21:26:13 +0900

    Refactor: 유저가 지치지 않는 ui/ux

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -10,6 +10,12 @@ import { updateApplyCard } from '@remote/apply'
 import { useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
+const STATUS_MESSAGE = {
+  [APPLY_STATUS.READY]: '카드 심사를 준비하고 있습니다.',
+  [APPLY_STATUS.PROGRESS]: '카드를 심사중입니다. 잠시만 기다려주세요.',
+  [APPLY_STATUS.COMPLETE]: '카드신청이 완료되었습니다.',
+}
+
 export default function ApplyPage() {
   const navigate = useNavigate()
   const user = useUser()
@@ -38,7 +44,7 @@ export default function ApplyPage() {
   })
 
   const [readyToPoll, setReadyToPll] = useState<boolean>(false)
-  usePollApplyStatus({
+  const { data: status } = usePollApplyStatus({
     onSuccess: async () => {
       await updateApplyCard({
         userId: user?.uid as string,
@@ -75,7 +81,7 @@ export default function ApplyPage() {
   }
 
   if (readyToPoll || isLoading) {
-    return <FullPageLoader message="카드를 신청 중입니다." />
+    return <FullPageLoader message={STATUS_MESSAGE[status ?? 'READY']} />
   }
 
   return <Apply onSubmit={mutate} />
